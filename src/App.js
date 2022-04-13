b1279c0f1103f8d1f4fb9a06faa0737a6113723e
LuckyShmucky <christianluciano567@gmail.com>
2022-04-13 11:29:27 -0700

    features/ suspense implimented without spinner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+// import Loader from 'react-loader-spinner'
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+// import AboutUs from './components/AboutUs
+//Components 
+
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
+  
+ 
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<h1 className='centerred'>Loading...</h1>}>
+          <div className='centerred'>
           <AboutUs />
           <Packages />
+          </div>
+        </Suspense>
       </div>
     </div>
   );
 }
-
 export default App;
